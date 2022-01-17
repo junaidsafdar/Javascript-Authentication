603e32f5e5cbc5e4a05c59352c2aec17ac5f7cc3
junaidjd1 <junaidsafdar54@gmail.com>
2022-01-18 00:29:27 +1100

    hashing password

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,9 +22,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//secret
-// const secret = 'Thisisourlittlesecret.';
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 const User = mongoose.model("user", userSchema);
 
 app.get("/", function (req, res) {
@@ -41,7 +37,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password),
     });
     newUser.save(function (err){
         if(err) {console.log(err)}
@@ -52,7 +48,7 @@ app.post("/register", function (req, res) {
 app.post("/login", function (req, res){
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({username: username},function (err, foundUser){
     if(err){
